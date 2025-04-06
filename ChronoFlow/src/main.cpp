840227950b6dccc4ce781deb97a14a8544431051
Alexander Ivanov <asivanov22@codingburgas.bg>
2025-04-06 20:32:27 +0300

    Creat Event.h  and start working on the main.cpp

--- ChronoFlow/src/main.cpp
+++ ChronoFlow/src/main.cpp
@@ -1,7 +1,37 @@
-#include "../include/MainMenu.h"
+#include <iostream>
+#include "../Include/MainMenu.h"
 
-int main() 
+int main()
 {
+	
+    int choice;
+
+    bool running = true;
+
     displayMenu();
-    return 0;
+
+	std::cin >> choice;
+
+
+    while (running)
+    {
+        switch (choice) {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            std::cout << "Exiting...\n";
+            running = false;
+            break;
+        default:
+            std::cout << "Invalid choice.\n";
+        }
+    }
 }
