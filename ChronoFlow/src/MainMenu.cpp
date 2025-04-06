96d3562583b28d968e7c5966cb81c1436efbd615
Alexander Ivanov <asivanov22@codingburgas.bg>
2025-04-06 16:13:03 +0300

    Redesign the menu

--- ChronoFlow/src/MainMenu.cpp
+++ ChronoFlow/src/MainMenu.cpp
@@ -3,11 +3,16 @@
 
 void displayMenu() {
     std::cout << "\n==================== ChronoFlow ====================\n";
-    std::cout << "1. Add Event\n";
-    std::cout << "2. Display All Events\n";
-    std::cout << "3. Search Event\n";
-    std::cout << "4. Delete Event\n";
-    std::cout << "5. Exit\n";
-    std::cout << "==================== ChronoFlow ====================\n";
+    std::cout << "||                                                ||\n";
+    std::cout << "||                                                ||\n";
+    std::cout << "||  1. Add Event                                  ||\n";
+    std::cout << "||  2. Display All Events                         ||\n";
+    std::cout << "||  3. Search Events                              ||\n";
+    std::cout << "||  4. Delete Events                              ||\n";
+    std::cout << "||  5. Sort Events                                ||\n";
+    std::cout << "||  6. Exit                                       ||\n";
+    std::cout << "||                                                ||\n";
+    std::cout << "||                                                ||\n";
+    std::cout << "==================== ChronoFlow ====================\n" << "\n";
     std::cout << "Choose an option: ";
 }
